7aaa4f5cf3a13acc7539bc22f86d4693a049f18b
hrkvalvik <69058816+hrkvalvik@users.noreply.github.com>
2026-02-15 14:49:26 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+// ==========================================
+// SONIC PASSPORT CONFIGURATION
+// ==========================================
+// 1. Enter your Google Email Address here to restrict access:
+window.ALLOWED_EMAIL = "hrkvalvik@gmail.com"; 
+
+// 2. Paste your Firebase Configuration keys here:
+window.MANUAL_FIREBASE_CONFIG = {
+    apiKey: "AIzaSyAf1_vklUc4E2baO1FaSh-g-LR2RFS2f3k",
+  authDomain: "sonic-passport.firebaseapp.com",
+  projectId: "sonic-passport",
+  storageBucket: "sonic-passport.firebasestorage.app",
+  messagingSenderId: "191886599912",
+  appId: "1:191886599912:web:cdbc7c21d36981915fcb5d",
+  measurementId: "G-DKPGXKCWWY"
+};
